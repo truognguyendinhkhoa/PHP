320f01ccbbb95324cefa5d7c0994cdc931cff364
trương nguyễn đình khoa 60135847 <63198645+truognguyendinhkhoa@users.noreply.github.com>
2021-10-26 18:23:07 +0700

    Create UploadForm.php

---
+++ CD6/UploadForm.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <form method="POST" action="upload.php" enctype="multipart/form-data">
+            <input type="file" name ="ProductImg"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
